9eb962bc940d68e06c9f94532339f2f4ca1da743
Michal Rosiak <michalrosiak90@gmail.com>
2016-10-03 16:39:24 +0200

    Dodanie do zdalnego repo niezbednych plikow

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': 63,
+	'X-Auth-Token': '7a97c7ebcfb2ce5560d01657c9026ffd'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
